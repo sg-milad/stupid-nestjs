c8e50a36651112c842ca9ebe7a8c66c84ec6f7fc
milad <miladbachari14@gmail.com>
2025-05-10 17:37:47 +0330

    feat: update customer entity

--- src/domain/customer/entities/customer.entity.ts
+++ src/domain/customer/entities/customer.entity.ts
@@ -3,6 +3,8 @@ import { CustomerCreatedEvent } from "../events/customer-created.event";
 import { PhoneNumber } from "../value-objects/phone-number.value-object";
 import { Email } from "../value-objects/email.value-object";
 import { BankAccount } from "../value-objects/bank-account.value-object";
+import { CustomerUpdatedEvent } from "../events/customer-update.event";
+import { CustomerDeletedEvent } from "../events/delete-customer.event";
 
 export class Customer extends AggregateRoot {
     private readonly id: string;
@@ -56,6 +58,38 @@ export class Customer extends AggregateRoot {
         );
 
         return customer;
+    }
+
+    public update(
+        firstName: string,
+        lastName: string,
+        dateOfBirth: Date,
+        phoneNumber: PhoneNumber,
+        email: Email,
+        bankAccountNumber: BankAccount,
+    ): void {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.bankAccountNumber = bankAccountNumber;
+
+        this.apply(
+            new CustomerUpdatedEvent(
+                this.id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                phoneNumber.getValue(),
+                email.getValue(),
+                bankAccountNumber.getValue(),
+            ),
+        );
+    }
+
+    public delete(): void {
+        this.apply(new CustomerDeletedEvent(this.id));
     }
 
     public getId(): string {
