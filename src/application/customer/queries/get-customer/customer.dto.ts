0eb327af68b17e200937958f5031ee8e394c5cfc
milad <miladbachari14@gmail.com>
2025-05-10 11:34:20 +0330

    feat: add customer query

---
+++ src/application/customer/queries/get-customer/customer.dto.ts
@@ -0,0 +1,27 @@
+export class CustomerDto {
+    id: string;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+    phoneNumber: string;
+    email: string;
+    bankAccountNumber: string;
+
+    constructor(
+        id: string,
+        firstName: string,
+        lastName: string,
+        dateOfBirth: Date,
+        phoneNumber: string,
+        email: string,
+        bankAccountNumber: string,
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.bankAccountNumber = bankAccountNumber;
+    }
+}
