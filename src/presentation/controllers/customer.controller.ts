7879576b730b60311b07fbc34bc6c0c89d858b9f
milad <miladbachari14@gmail.com>
2025-05-10 17:41:24 +0330

    feat: update the controller and add update getall delete

--- src/presentation/controllers/customer.controller.ts
+++ src/presentation/controllers/customer.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Get, Param, Post, BadRequestException, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, BadRequestException, UsePipes, ValidationPipe, Delete, Put } from "@nestjs/common";
 import { CommandBus, QueryBus } from "@nestjs/cqrs";
-import { ApiTags, ApiOperation, ApiResponse, ApiParam } from "@nestjs/swagger";
-import { CreateCustomerCommand } from "../../application/customer/commands/create-customer/create-customer.command";
-import { GetCustomerQuery } from "../../application/customer/queries/get-customer/get-customer.query";
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiOkResponse, ApiNoContentResponse } from "@nestjs/swagger";
+import { CreateCustomerCommand } from "../../application/customer/commands/create-customer.command";
+import { GetCustomerQuery } from "../../application/customer/queries/get-customer.query";
 import { CreateCustomerRequestDto } from "../dtos/create-customer.request.dto";
 import { CustomerResponseDto } from "../dtos/customer.response.dto";
-import { CustomerDto } from "../../application/customer/queries/get-customer/customer.dto";
 import { PhoneValidatorService } from "../../infrastructure/validation/phone-validator.service";
+import { CustomerDto } from "src/application/customer/queries/customer.dto";
+import { GetAllCustomersQuery } from "src/application/customer/queries/get-all-customer.query";
+import { DeleteCustomerCommand } from "src/application/customer/commands/delete-customer.command";
+import { UpdateCustomerCommand } from "src/application/customer/commands/update-customer.command";
+import { UpdateCustomerRequestDto } from "../dtos/update-customer.request.dto";
 
 @ApiTags("customers")
 @Controller("customers")
@@ -15,7 +19,7 @@ export class CustomerController {
         private readonly commandBus: CommandBus,
         private readonly queryBus: QueryBus,
         private readonly phoneValidatorService: PhoneValidatorService,
-    ) {}
+    ) { }
 
     @Post()
     @ApiOperation({ summary: "Create a new customer" })
@@ -61,17 +65,79 @@ export class CustomerController {
     })
     @ApiResponse({ status: 404, description: "Customer not found" })
     async getCustomer(@Param("id") id: string): Promise<CustomerResponseDto> {
-        const customerDto: CustomerDto = await this.queryBus.execute(new GetCustomerQuery(id));
-        const dob = new Date(customerDto.dateOfBirth);
+        const customerDto = await this.queryBus.execute(new GetCustomerQuery(id));
         const response = new CustomerResponseDto();
+
         response.id = customerDto.id;
         response.firstName = customerDto.firstName;
         response.lastName = customerDto.lastName;
-        response.dateOfBirth = dob.toISOString().split("T")[0];
+        response.dateOfBirth = new Date(customerDto.dateOfBirth);
         response.phoneNumber = customerDto.phoneNumber;
         response.email = customerDto.email;
         response.bankAccountNumber = customerDto.bankAccountNumber;
 
+        return response;
+    }
+
+    @Get()
+    @ApiOkResponse({
+        description: 'List of all customers',
+        type: [CustomerResponseDto]
+    })
+    async getAllCustomers(): Promise<CustomerResponseDto[]> {
+        const query = new GetAllCustomersQuery();
+        const customerDtos = await this.queryBus.execute<GetAllCustomersQuery, CustomerDto[]>(query);
+
+        return customerDtos.map(customerDto => this.mapToResponseDto(customerDto));
+    }
+
+    @Delete(":id")
+    @ApiParam({ name: 'id', description: 'Customer ID' })
+    @ApiNoContentResponse({
+        description: 'Customer has been deleted successfully'
+    })
+    async deleteCustomer(@Param("id") id: string): Promise<void> {
+        const command = new DeleteCustomerCommand(id);
+        await this.commandBus.execute(command);
+    }
+
+    @Put(":id")
+    @ApiParam({ name: 'id', description: 'Customer ID' })
+    @ApiOkResponse({
+        description: 'Customer has been updated successfully',
+        type: CustomerResponseDto
+    })
+    async updateCustomer(
+        @Param("id") id: string,
+        @Body() updateCustomerDto: UpdateCustomerRequestDto
+    ): Promise<CustomerResponseDto> {
+        const command = new UpdateCustomerCommand(
+            id,
+            updateCustomerDto.firstName,
+            updateCustomerDto.lastName,
+            new Date(updateCustomerDto.dateOfBirth),
+            updateCustomerDto.phoneNumber,
+            updateCustomerDto.email,
+            updateCustomerDto.bankAccountNumber,
+        );
+
+        await this.commandBus.execute(command);
+
+        const query = new GetCustomerQuery(id);
+        const customerDto = await this.queryBus.execute<GetCustomerQuery, CustomerDto>(query);
+
+        return this.mapToResponseDto(customerDto);
+    }
+
+    private mapToResponseDto(customerDto: CustomerDto): CustomerResponseDto {
+        const response = new CustomerResponseDto();
+        response.id = customerDto.id;
+        response.firstName = customerDto.firstName;
+        response.lastName = customerDto.lastName;
+        response.dateOfBirth = new Date(customerDto.dateOfBirth);
+        response.phoneNumber = customerDto.phoneNumber;
+        response.email = customerDto.email;
+        response.bankAccountNumber = customerDto.bankAccountNumber;
         return response;
     }
 }
