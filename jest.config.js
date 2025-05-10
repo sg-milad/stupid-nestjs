140f7812655511259ec3fbd029c65a0cc512a0ac
milad <miladbachari14@gmail.com>
2025-05-10 14:37:56 +0330

    fix: test congfig

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: ".",
+    // only look for tests under the "test" folder:
+    roots: ["<rootDir>/test"],
+    // match both *.spec.ts and *.e2e-spec.ts
+    testMatch: ["**/?(*.)+(spec|e2e-spec).ts"],
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest",
+    },
+    testEnvironment: "node",
+};
