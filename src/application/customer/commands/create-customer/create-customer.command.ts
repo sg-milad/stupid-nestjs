fcbbda30f80c166a8ce9f4d0d27de0203cfe550e
milad <miladbachari14@gmail.com>
2025-05-10 11:23:05 +0330

    feat: add create customer command & dto

---
+++ src/application/customer/commands/create-customer/create-customer.command.ts
@@ -0,0 +1,10 @@
+export class CreateCustomerCommand {
+    constructor(
+        public readonly firstName: string,
+        public readonly lastName: string,
+        public readonly dateOfBirth: Date,
+        public readonly phoneNumber: string,
+        public readonly email: string,
+        public readonly bankAccountNumber: string,
+    ) { }
+}
